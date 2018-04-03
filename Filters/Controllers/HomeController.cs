ca7069a9ac4e946cf69f179761914b0bb55c2bb6
Peter <peter.prentiss@gmail.com>
2018-04-03 12:00:18 -0500

    Create httpsonly filter

--- Filters/Controllers/HomeController.cs
+++ Filters/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Filters.Infrastructure;
 
 namespace Filters.Controllers
 {
+    [HttpsOnly]
     public class HomeController : Controller
     {
+        [RequireHttps]
         public ViewResult Index() => View("Message",
             "This is the Index action on the Home controller");
     }
